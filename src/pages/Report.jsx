65ecf9d9181cb3075543539df445d98acce98839
EPolyz <elias.polyzos@gmail.com>
2024-01-21 09:48:57 -0500

    Finalizing reports

--- src/pages/Report.jsx
+++ src/pages/Report.jsx
@@ -13,11 +13,19 @@ import axios from "axios";
 Chart.register(CategoryScale);
 export default function App() {
 
-  const { data: revenueByTypeAndMonth, isLoading, error } = useQuery(
+  const { data: revenueByTypeAndMonth } = useQuery(
     'accepted-dict',
     () => axios.get('http://localhost:8080/api/management/getAcceptedDictionary'),
     {
-      select: response => response.data // To directly access the data object in the response
+      select: response => response.data, // To directly access the data object in the response
+    }
+  );
+
+  const { data: lostRevenueByTypeAndMonth } = useQuery(
+    'denied-dict',
+    () => axios.get('http://localhost:8080/api/management/getDeniedDictionary'),
+    {
+      select: response => response.data, // To directly access the data object in the response
     }
   );
 
@@ -27,7 +35,8 @@ export default function App() {
       <Card className="items-center w-full">
         <div className="w-full flex-row flex justify-center">
 
-          {Object.keys(revenueByTypeAndMonth).map(key => {
+          {/* {
+          revenueByTypeAndMonth&&Object.keys(revenueByTypeAndMonth).map(key => {
             const item = revenueByTypeAndMonth[key];
             return (
               <div key={key}>
@@ -38,17 +47,17 @@ export default function App() {
                 <p>Label: {item.label}</p>
               </div>
             );
-          })}
+          })} */}
 
 
           <Card className="max-w-sm">
             <PolarArea
               data={{
-                labels: revenueData.map((data) => data.label),
+                labels: revenueByTypeAndMonth&&Object.keys(revenueByTypeAndMonth).map((data) => revenueByTypeAndMonth[data].label),
                 datasets: [
                   {
-                    label: "Count",
-                    data: revenueData.map((data) => data.count),
+                    label: 'Revenue',
+                    data: revenueByTypeAndMonth&&Object.keys(revenueByTypeAndMonth).map((data) => revenueByTypeAndMonth[data].revenue),
                     backgroundColor: [
                       "rgba(43, 63, 229, 0.8)",
                       "rgba(250, 192, 19, 0.8)",
@@ -65,11 +74,53 @@ export default function App() {
           <Card className="max-w-sm">
             <PolarArea
               data={{
-                labels: revenueData.map((data) => data.label),
+                labels: revenueByTypeAndMonth&&Object.keys(revenueByTypeAndMonth).map((data) => revenueByTypeAndMonth[data].label),
                 datasets: [
                   {
-                    label: "Count",
-                    data: revenueData.map((data) => data.revenue),
+                    label: 'Quantity(Revenue)',
+                    data: revenueByTypeAndMonth&&Object.keys(revenueByTypeAndMonth).map((data) => revenueByTypeAndMonth[data].qty),
+                    backgroundColor: [
+                      "rgba(43, 63, 229, 0.8)",
+                      "rgba(250, 192, 19, 0.8)",
+                      "rgba(253, 135, 135, 0.8)",
+                      "rgba(120, 192, 19, 0.8)",
+                      "rgba(203, 35, 135, 0.8)",
+                    ],
+                    borderRadius: 1,
+                  },
+                ],
+              }}
+            />
+          </Card>
+          <Card className="max-w-sm">
+            <PolarArea
+              data={{
+                labels: lostRevenueByTypeAndMonth&&Object.keys(lostRevenueByTypeAndMonth).map((data) => lostRevenueByTypeAndMonth[data].label),
+                datasets: [
+                  {
+                    label: "Revenue Lost",
+                    data: lostRevenueByTypeAndMonth&&Object.keys(lostRevenueByTypeAndMonth).map((data) => lostRevenueByTypeAndMonth[data].loss),
+                    backgroundColor: [
+                      "rgba(43, 63, 229, 0.8)",
+                      "rgba(250, 192, 19, 0.8)",
+                      "rgba(253, 135, 135, 0.8)",
+                      "rgba(120, 192, 19, 0.8)",
+                      "rgba(203, 35, 135, 0.8)",
+                    ],
+                    borderRadius: 1,
+                  },
+                ],
+              }}
+            />
+          </Card>
+          <Card className="max-w-sm">
+            <PolarArea
+              data={{
+                labels: lostRevenueByTypeAndMonth&&Object.keys(lostRevenueByTypeAndMonth).map((data) => lostRevenueByTypeAndMonth[data].label),
+                datasets: [
+                  {
+                    label: "Quantity(Revenue Lost)",
+                    data: lostRevenueByTypeAndMonth&&Object.keys(lostRevenueByTypeAndMonth).map((data) => lostRevenueByTypeAndMonth[data].qty),
                     backgroundColor: [
                       "rgba(43, 63, 229, 0.8)",
                       "rgba(250, 192, 19, 0.8)",
